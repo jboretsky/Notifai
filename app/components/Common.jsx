2ff3b4908e5cbbac06543e500ccf929fe25f8f0e
Ben Hawker <ben@hawker.cc>
2016-10-16 02:15:33 -0700

    Remove class

--- app/components/Common.jsx
+++ app/components/Common.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export class CalendarBodyWrapper extends React.Component {
   render() {
     return (
-      <div className="calendar-body">
+      <div>
         {this.props.children}
       </div>
     );
